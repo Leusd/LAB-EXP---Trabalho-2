6e6e8e7a49cb57eeb5b847aa924eb2e4ceff0324
talitaam <talitaarantes@yahoo.com.br>
2020-03-24 00:45:14 -0300

    Sprint 1 finished

--- Script 1 - Lab 2.py
+++ Script 1 - Lab 2.py
@@ -2,9 +2,15 @@ import requests
 import json
 import time
 import os
+import shutil
+import errno
+import stat
+import subprocess
+from radon.raw import analyze
+from pygit2 import clone_repository
 
 
-headers = {"Authorization": "Bearer  Your key here"}
+headers = {"Authorization": "Bearer Your key here"}
 
 
 def run_query(json, headers): #Função que executa uma request pela api graphql 
@@ -19,18 +25,18 @@ def run_query(json, headers): #Função que executa uma request pela api graphql
 
 
 #Respostas das perguntas na Query:
-#Questão 1 - Métrica: idade do repositório - "nameWithOwner" e "createdAt"
-#Questão 2 - Métrica: total de pull requests aceitas - "questão 1 + pullRequests(states: MERGED){totalCount}" 
-#Questão 3 - Métrica: total de releases - "releases{totalCount}"
-#Questão 4 - Métrica: tempo até a última atualização - "updatedAt"
-#Questão 5 - Métrica: linguagem primária de cada um desses repositórios - "questão 4 + primaryLanguage{name}"
-#Questão 5 - Métrica: razão entre número de issues fechadas pelo total de issues - "closedIssues : issues(states: CLOSED){totalCount}" / "totalIssues: issues{totalCount}"
+#Questão 1 - 
+#Questão 2 - 
+#Questão 3 - 
+#Questão 4 - 
+#Questão 5 - 
+#Questão 5 - 
 
 
 query = """
 query example{  
           user(login: "gvanrossum") {
-              repositories(first: 100) {
+              repositories(first: 100, isFork: false) {
       			totalCount
                 nodes {
                   nameWithOwner
@@ -61,12 +67,13 @@ query example{
 """
 
 
-print("\n\n ------------- Começo da consulta com graphQL para responder as Questões de 1 a 6 ------------- \n" )
+print("\n\n ------------- Começo da consulta com graphQL ------------- \n" )
 
 #finalQuery = query.replace("{AFTER}", "")
 
 
 json={"query": query}
+
 
 #total_pages = 1
 
@@ -75,7 +82,7 @@ result = run_query(json, headers)
 #print(result)
 nodes = result['data']['user']['repositories']['nodes']
 totalRepos = str(result['data']['user']['repositories']['totalCount'])
-print("total repositorios =" + totalRepos + "\n\n")
+print("total repositorios encontrados = " + totalRepos + "\n")
 #next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
 
 #paginating
@@ -89,7 +96,7 @@ print("total repositorios =" + totalRepos + "\n\n")
 #    next_page  = result["data"]["search"]["pageInfo"]["hasNextPage"]
 
 
-print("\n\n ------------- Retorno da consulta com graphQL para responder as Questões de 1 a 6 ------------- \n" )
+#print("\n\n ------------- Retorno da consulta com graphQL para responder as Questões de 1 a 6 ------------- \n" )
 #print(nodes)
 
 #saving data
@@ -97,20 +104,88 @@ print("\n\n ------------- Retorno da consulta com graphQL para responder as Ques
 if os.path.exists("repos.csv"):
   os.remove("repos.csv")
 
+
+def on_rm_error(func, path, exc_info):
+    # path contains the path of the file that couldn't be removed
+    # let's just assume that it's read-only and unlink it.
+    os.chmod(path, stat.S_IWRITE )
+    os.unlink(path)
+
+def cleanRepository(folder):
+  for filename in os.listdir(folder):
+      file_path = os.path.join(folder, filename)
+      try:
+          if os.path.isfile(file_path) or os.path.islink(file_path):
+              os.unlink(file_path)
+          elif os.path.isdir(file_path):
+              shutil.rmtree(file_path, onerror = on_rm_error)
+      except Exception as e:
+          print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+numRepo = 0
+
+print("\n ------ Começo leitura repositorios ------ \n" )
+
 with open("repos.csv", 'w') as the_file:
         the_file.write("nameWithOwner" + ";" + "url" + ";" + "stars" + ";" 
         + "watchers" + ";" + "forks" + ";" + "releases" + ";" 
-        + "createdAt" + ";" + "primaryLanguage" + "\n")
+        + "createdAt" + ";" + "primaryLanguage" + ";" + "totalLOC" +"\n")
 for node in nodes:
     if node['primaryLanguage'] is not None:
-        if node['primaryLanguage']['name'] == "Python": #and  node['owner']['login']=="gvanrossum"
+        if node['primaryLanguage']['name'] == "Python" and node['nameWithOwner'] != "fake-python/cpython":
+          repo_path = 'Repository/' + str(numRepo)
+          if(os.path.exists(repo_path)):
+            cleanRepository(repo_path)
+            print("\n" + "Limpeza da pasta do Repositório: ")
+          print("repo_path = " + repo_path)
+          numRepo += 1
+          #print("\n" + "Comeca limpeza")
+          #cleanRepository(repo_path)
+          time.sleep(1)
+          #if os.path.exists("Repository/.git"):
+           # print("\n entrou no if do apagar")
+            #for root, dirs, files in os.walk('Repository/.git'):
+             # for fname in files:
+              #  full_path = os.path.join(root, fname)
+               # print("\n Tentando apagar o " + full_path)
+                #os.chmod(full_path, stat.S_IWRITE)
+                #os.remove(full_path)      
+              #for dir in dirs:
+               # full_path = os.path.join(root, dir)
+                #subprocess.check_call(('attrib -R ' + full_path + '\\* /S').split())
+                #cleanRepository(full_path)
+            #cleanRepository("Repository/.git")
+            #cleanRepository("Repository/.git/objects/pack")
+          #cleanRepository(repo_path)
+          totalLoc = 0
+          gitURL = node['url'] + ".git"
+          print("\n" + "Começa o git clone")
+          print(node['url'])
+          repo = clone_repository(gitURL, repo_path)
+          print("Termina o git clone \n")
+          for root, dirs, files in os.walk(repo_path):
+            for file in files:
+              if file.endswith('.py'):
+                fullpath = os.path.join(root, file)
+                with open(fullpath, encoding="utf8") as f:
+                  content = f.read()
+                  print("\nLendo arquivo " + fullpath)
+                  b = analyze(content)
+                  print(b)
+                  i = 0
+                  for item in b:
+                    if i==0:
+                      totalLoc += item
+                      print("loc = " + str(item))
+                      print("Total loc até agora: " + str(totalLoc))
+                      i+=1
+          print("\nTotal loc final para o repo " + node['nameWithOwner'] + " é " + str(totalLoc))
+          print("\n ------ Fim de um repositorio ------ \n" )
+          cleanRepository(repo_path)
           with open("repos.csv", 'a') as the_file:
                 the_file.write(node['nameWithOwner'] + ";" + node['url'] + ";" + str(node['stargazers']['totalCount'])
                 + ";" + str(node['watchers']['totalCount']) + ";" + str(node['forks']['totalCount']) + ";"
-                + str(node['releases']['totalCount']) + ";" + node['createdAt'] + ";" + node['primaryLanguage']['name'] + ";" + node['owner']['login'] +  "\n") 
-
-
-
-
-
-print("\n\n ------------- Retorno do print com graphQL para responder as Questões de 1 a 6 ------------- \n" )
+                + str(node['releases']['totalCount']) + ";" + node['createdAt'] + ";" + node['primaryLanguage']['name'] + ";" + str(totalLoc) +  "\n") 
+          
+print("Total repositórios " + str(numRepo))
+print("\n ------------- Fim da execução ------------- \n" )
